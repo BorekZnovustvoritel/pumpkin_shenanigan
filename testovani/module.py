d0e623a83dacb39189b74b7ba64f6cedf00dd189
BorekZnovustvoritel <tvojemamatemarada@seznam.cz>
2021-07-28 10:16:14 +0200

    Test #3 - guild tests

--- testovani/module.py
+++ testovani/module.py
@@ -1,4 +1,6 @@
 import discord
+import tempfile
+import json
 from discord.ext import commands
 
 from core import acl, text, logging
@@ -8,28 +10,61 @@ class Testovani(commands.Cog):
         self.bot = bot
 
     @commands.command()
+    async def rawGr(self, ctx):
+        await ctx.send(ctx.guild.by_category)
+
+
+
+    @commands.command()
     async def groups(self, ctx):
+
+
+
+        if len(ctx.message.attachments) != 1:
+            await ctx.send("Only 1 file allowed as an attachment.")
+            return
+        if not ctx.message.attachments[0].filename.lower.endswith("json"):
+            await ctx.send("Only .json files allowed as an attachment.")
+            return
+        # download the file
+        # similiar code that was used for a reference: main/modules/base/acl/module.py/line 276
+        data_file = tempfile.TemporaryFile()
+        await ctx.message.attachments[0].save(data_file)
+        data_file.seek(0)
+        try:
+            json_data = json.load(data_file)
+        except json.decoder.JSONDecodeError:
+            await ctx.send("This file seems corrupted.")
+            return
+        # loading data from .json file
+        institutes = []
+        for subject in json_data:
+            if subject['institute'] not in institutes:
+                institutes.append(subject)
+
         categories = ctx.guild.by_category()
         categoryNames = []
         channelNames = []
         for category in categories:
-            categoryName = category[0]
+            categoryName = str(category[0]).lower()
             if categoryName not in categoryNames:
-                categoryNames.append(str(categoryName))
+                if categoryName in institutes:
+                    categoryNames.append(categoryName)
             channels = category[1]
             for channel in channels:
-                channelNames.append(str(channel))
-            #if header['name'] not in categoryNames:
-            #    categoryNames.append(header['name'])
-            #rooms = category[1]
-            #for room in rooms:
-            #    if room[1] not in names:
-            #        names.append(room[1])
-        await ctx.send(str(categoryNames)+str(channelNames))
+                channelNames.append(str(channel).lower())
 
-    #@commands.command()
-    #async def channels(self, ctx):
-    #    await ctx.send(ctx.guild.fetch_channels())
+
+        for institute in institutes:
+            names = []
+            abbrs = []
+            for subject in institute:
+                if subject['name'] not in names:
+                    names.append(subject['name'].lower())
+                if subject['abbreviation'] not in abbrs:
+                    abbrs.append(subject['abbreviation'].lower())
+        # loading data from guild
+        await ctx.send(str(categoryNames)+str(channelNames))
 
 def setup(bot) -> None:
     bot.add_cog(Testovani(bot))
