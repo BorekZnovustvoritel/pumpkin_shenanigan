acfcb35c65e1ac50c1ea3d3061784f3ee39d0459
BorekZnovustvoritel <tvojemamatemarada@seznam.cz>
2021-07-28 12:15:43 +0200

    Test #3 - guild tests

--- testovani/module.py
+++ testovani/module.py
@@ -80,12 +80,11 @@ class Testovani(commands.Cog):
                 if str(category[0]).lower() == institute:
                     categoryFound = True
                     break
-            print("Category: "+str(category[0]))
             if not categoryFound:
                 await ctx.send("Category %s not found on this server." % institute)
                 continue
             channels = category[1]
-            #print("Channels: "+str(channels))
+            print("Channels: "+str(channels))
             chNames = []
             chDescr = []
             for channel in channels:
