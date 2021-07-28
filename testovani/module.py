7902ebba25398159335edfabb7586ab04a1a5541
BorekZnovustvoritel <tvojemamatemarada@seznam.cz>
2021-07-28 11:55:07 +0200

    Test #3 - guild tests

--- testovani/module.py
+++ testovani/module.py
@@ -82,7 +82,7 @@ class Testovani(commands.Cog):
                     break
             print("Category: "+str(category))
             if not categoryFound:
-                await ctx.send("Category %s not found on this server." % str(category[0]).lower())
+                await ctx.send("Category %s not found on this server." % institute)
             channels = category[1]
             #print("Channels: "+str(channels))
             chNames = []
